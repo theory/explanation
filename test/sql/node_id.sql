436c2712fb11b8341efe210815f2d16d24044628
David E. Wheeler <david@justatheory.com>
2010-11-06 14:48:31 -0700

    Test Node ID generation.

---
+++ test/sql/node_id.sql
@@ -0,0 +1,19 @@
+\set ECHO 0
+\set QUIET 1
+BEGIN;
+\i sql/explain-table.sql
+
+-- Mock clock_timestamp().
+CREATE SCHEMA mock;
+CREATE OR REPLACE FUNCTION mock.clock_timestamp(
+) RETURNS timestamptz LANGUAGE SQL AS $$
+    SELECT '2010-07-19 11:01:03.306399+00'::timestamptz;
+$$;
+
+SET search_path = mock,public,pg_catalog;
+
+SELECT "Node ID" = md5(pg_backend_pid()::text || clock_timestamp())
+    AS "Node ID is PID || clock timestamp"
+  FROM parse_node('<Plan><Node-Type>Aggregate</Node-Type></Plan>');
+
+ROLLBACK;
